0d2c8daf2f5641b4a4401e355576cdd2a247da3d
Zayden <danechka.elkin@gmail.com>
2023-09-09 23:08:20 +0500

    добавлен QTcpSocket

--- server.cpp
+++ server.cpp
@@ -1,18 +1,33 @@
 #include "server.h"
 
-Server::Server()
-{
-
-}
+Server::Server() {}
 
 void Server::startServer()
 {
+    if (this->listen(QHostAddress("127.0.0.1"), 5555))
+    {
+        qDebug() << "Server started";
+
+    }
+    else
+    {
+        qDebug() << "Server not started";
+    }
 
 }
 
 void Server::incomingConnection(qintptr socket_descriptor)
 {
+    socket = new QTcpSocket(this);
+    socket->setSocketDescriptor(socket_descriptor);
+    connect(socket, SIGNAL(readyRead()), this, SLOT(socketReady()));
+    connect(socket, SIGNAL(disconnected()), this, SLOT(socketDisconnect()));
 
+    qDebug() << socket_descriptor << " is connected";
+
+    socket->write("you are connected");
+
+    qDebug() << "write to client: you are connected";
 }
 
 void Server::socketReady()
@@ -22,5 +37,6 @@ void Server::socketReady()
 
 void Server::socketDisconnect()
 {
-
+    qDebug() << "smb is disconnected";
+    socket->deleteLater();
 }
